74e005dfaa0e9b4f1da441b1dad23e0f2b81549d
EzyhB <ezyhbmail@gmail.com>
2022-06-09 22:06:23 +0100

    Navbar Added

--- epic-app-gnews/src/components/App/index.tsx
+++ epic-app-gnews/src/components/App/index.tsx
@@ -5,12 +5,14 @@ import { useState } from "react";
 import MainDisplay from "../MainDisplay";
 import { Container } from "../styles/Container";
 import { lightTheme, darkTheme } from "../styles/Theme";
+import Navbar from "../Navbar";
 
 function App() {
   const [isLight, setIsLight] = useState(false);
   return (
     <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
       <GlobalStyles />
+      <Navbar isLight={isLight} setIsLight={setIsLight} />
       <Container>
         <MainDisplay />
       </Container>
